736652e06a39fc76ac6b2ecf2330053c63d53e18
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-10-12 01:07:43 -0300

    fix: deploy

--- procfile
+++ procfile
@@ -1,1 +1,1 @@
-web: node dist/src/server.js
+web: ts-node/src/server.js
--- src/database.ts
+++ src/database.ts
@@ -2,27 +2,3 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export default prisma
-
-// async function main() {
-//   console.log("entrou")
-
-//   const newAsset = await prisma.asset.create({
-//     data: {
-//       name: "Empacotadora",
-//       image: "http://www.euromax.com.br/imagens/maquinas/maquina-embalagem-produto.jpg",
-//       model: "FE25963br",
-//       owner: "Freios Supremos",
-//       description: "Embalagem do material",
-//       status: "Stopped",
-//       healthLevel: 20,
-//       unit: {
-//         connect: {
-//           id: "633fa56f1dfe7c254ac8e9bc"
-//         }
-//       }
-//     }
-//   })
-//   console.log(newAsset)
-// }
-
-// main()
